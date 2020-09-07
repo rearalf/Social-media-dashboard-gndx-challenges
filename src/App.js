5e1dfc67d2ff691324c054427095fdb63f498e75
rearalf <rear_recinos@hotmail.com>
2020-09-07 16:37:31 -0600

    Todo funciona

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+const App = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	);
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+					},
+				],
+			},
+			{
+				test: /\.css$/i,
+				use: [ 'style-loader', 'css-loader' ],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				loader: 'file-loader',
+				options: {
+					outputPath: 'assets/img',
+					name: '[name].[ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+			favicon: './public/favicon.png',
+		}),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 3000,
+		open: true,
+	},
+};
